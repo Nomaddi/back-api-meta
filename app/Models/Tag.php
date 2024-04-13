c6affd5d9eccea0c3bb7849d5bb935361863708b
jhonrymat <jhonrymat@gmail.com>
2024-04-12 21:47:26 -0500

    comparando con webhook maddigo

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+    protected $fillable = ['nombre', 'descripcion', 'color'];  // Asegúrate de incluir todos los campos que deseas asignar masivamente
+
     public function contactos(){
         return $this->belongsToMany(Contacto::class, 'contacto_tag', 'tag_id', 'contacto_id');
     }
