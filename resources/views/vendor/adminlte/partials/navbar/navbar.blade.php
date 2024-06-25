bacd2aa037634d96c1892e923c217c99d91bf7a7
jhonrymat <jhonrymat@gmail.com>
2024-06-24 22:47:14 -0500

    notificaciones para el rol cl

--- resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
@@ -1,4 +1,5 @@
-<nav class="main-header navbar
+<nav
+    class="main-header navbar
     {{ config('adminlte.classes_topnav_nav', 'navbar-expand') }}
     {{ config('adminlte.classes_topnav', 'navbar-white navbar-light') }}">
 
@@ -22,9 +23,38 @@
         {{-- Configured right links --}}
         @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
 
+        @if (auth()->user()->hasRole('ContratacionL'))
+            <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+            <script>
+                function subscribeToPusher() {
+                    var pusher = new Pusher('52c212ce563c5534e98c', { // Usa tu clave real aquí
+                        cluster: 'us2' // Usa tu cluster real aquí
+                    });
+                    var channel = pusher.subscribe('my-channel');
+                    channel.bind('my-event', function(data) {
+                        new Notification('Nuevo Contrato', {
+                            body: `La empresa ${data.data.empresa} acaba de publicar un contrato.`,
+                            icon: '{{ asset('vendor/adminlte/dist/img/maddi-go.png') }}' // Asegúrate de poner la ruta correcta del icono
+                        });
+                    });
+                }
+
+                if (Notification.permission === "granted") {
+                    subscribeToPusher();
+                } else if (Notification.permission !== "denied") {
+                    Notification.requestPermission().then(permission => {
+                        if (permission === "granted") {
+                            subscribeToPusher();
+                        }
+                    });
+                }
+            </script>
+        @endif
+
+
         {{-- User menu link --}}
-        @if(Auth::user())
-            @if(config('adminlte.usermenu_enabled'))
+        @if (Auth::user())
+            @if (config('adminlte.usermenu_enabled'))
                 @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
             @else
                 @include('adminlte::partials.navbar.menu-item-logout-link')
@@ -32,7 +62,7 @@
         @endif
 
         {{-- Right sidebar toggler link --}}
-        @if(config('adminlte.right_sidebar'))
+        @if (config('adminlte.right_sidebar'))
             @include('adminlte::partials.navbar.menu-item-right-sidebar-toggler')
         @endif
     </ul>
